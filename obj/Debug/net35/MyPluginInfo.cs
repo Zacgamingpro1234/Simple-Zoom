97826d42f12f704e46a53d5d77194a0c9e6a7403
Zacgamingpro1234 <zac.vella.mt@gmail.com>
2025-03-19 17:32:02 +0100

    ig bad

--- obj/Debug/net35/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace SimpleZoom
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "SimpleZoom";
-        public const string PLUGIN_NAME = "Simple Zoom";
-        public const string PLUGIN_VERSION = "1.1";
-    }
-}
