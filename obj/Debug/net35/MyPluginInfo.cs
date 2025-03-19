c632ae0d3f15345fe7cef12f45a0a0c67b0248fd
Zacgamingpro1234 <zac.vella.mt@gmail.com>
2025-03-19 16:55:00 +0100

    forgor these

---
+++ obj/Debug/net35/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace SimpleZoom
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "SimpleZoom";
+        public const string PLUGIN_NAME = "Simple Zoom";
+        public const string PLUGIN_VERSION = "1.1";
+    }
+}
